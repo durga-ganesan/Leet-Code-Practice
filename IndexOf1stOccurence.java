5379473da2dcd0a657e8bbcadf7c408b6af6a32c
durga-ganesan <durga05jo@gmail.com>
2025-06-06 12:16:30 +0530

    Update IndexOf1stOccurence.java

--- IndexOf1stOccurence.java
+++ IndexOf1stOccurence.java
@@ -1,5 +1,15 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+        int j=0;
+        for(int i=0;i<haystack.length();i++){
+            if(haystack.charAt(i)== needle.charAt(j)){
+                j++;
+                if(j==needle.length())  return i-j+1;
+            }else{
+                i=i-j;
+                j=0;
+            }
+        }
+        return -1;
     }
 }
